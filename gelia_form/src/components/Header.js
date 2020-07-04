a86d62853fc6b69454f4f500f9b3d54e58c3e031
roselandroche <r.landroche@gmail.com>
2020-07-04 17:28:33 -0400

    Edited Header style

--- gelia_form/src/components/Header.js
+++ gelia_form/src/components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../css/Header.css';
 
 export default function Header() {
     return (
-        <div>
+        <section className='header'>
             <h2>Gelia Wells and Mohr</h2>
             <h2>Direct</h2>
             <h2>FAQ</h2>
             <h2>Contact Support</h2>
-        </div>
+        </section>
     )
 }
