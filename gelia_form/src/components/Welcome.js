f21191d73c71df52d9ba33ea6f7f5c0fde5297e9
roselandroche <r.landroche@gmail.com>
2020-07-04 17:49:00 -0400

    Minor edits to content and style of Welcome

--- gelia_form/src/components/Welcome.js
+++ gelia_form/src/components/Welcome.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import LogInForm from './LogInForm'
+import LogInForm from './LogInForm';
+import '../css/Welcome.css';
 
 export default function Welcome() {
     return (
         <section>
+            <h3>Welcome! Please log in to continue...</h3>
             <LogInForm />
         </section>
     )
