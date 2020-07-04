7ec5495e56ea05055e58df72d7b7090f573de55b
roselandroche <r.landroche@gmail.com>
2020-07-04 17:30:23 -0400

    Minor edits to form and welcome components

--- gelia_form/src/components/LogInForm.js
+++ gelia_form/src/components/LogInForm.js
@@ -2,18 +2,16 @@ import React from 'react';
 
 export default function LogInForm () {
     return (
-        <>
-            <form>
-                <label>
-                    Username: 
-                    <input type='text' name='name' />
-                </label>
-                <label>
-                    Password: 
-                    <input type='text' name='password' />
-                </label>
-                <input type='submit' value='Submit' />
-            </form>
-        </>
+        <form>
+            <label>
+                Username: 
+                <input type='text' name='name' />
+            </label>
+            <label>
+                Password: 
+                <input type='text' name='password' />
+            </label>
+            <input type='submit' value='Submit' />
+        </form>
     )
 }
--- gelia_form/src/components/Welcome.js
+++ gelia_form/src/components/Welcome.js
@@ -4,7 +4,6 @@ import LogInForm from './LogInForm'
 export default function Welcome() {
     return (
         <section>
-            <header>Direct Log In</header>
             <LogInForm />
         </section>
     )
