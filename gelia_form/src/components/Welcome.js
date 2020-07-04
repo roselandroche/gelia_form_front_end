9284aa243f2f33ab1540d321b9f1c308eb5c1828
roselandroche <r.landroche@gmail.com>
2020-07-04 16:43:57 -0400

    Removing excess, starting to build components

---
+++ gelia_form/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LogInForm from './LogInForm'
+
+export default function Welcome() {
+    return (
+        <section>
+            <header>Gelia Direct</header>
+            {/* <a href='./LogIn.js'>Log In</a> */}
+        </section>
+    )
+}
